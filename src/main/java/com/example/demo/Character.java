b9dd232abd7245a1e91ab0014a8df54c0e6a1cb4
Kashyap <Niyatee.Kashyap@harman.com>
2021-10-14 13:32:09 +0530

    First Commit

---
+++ src/main/java/com/example/demo/Character.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+public class Character {
+	private String name;
+	private int max_power;
+	public Character(String name, int max_power) {
+		super();
+		this.name = name;
+		this.max_power = max_power;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMax_power() {
+		return max_power;
+	}
+	public void setMax_power(int max_power) {
+		this.max_power = max_power;
+	}
+	@Override
+	public String toString() {
+		return "Character [name=" + name + ", max_power=" + max_power + "]";
+	}
+	
+
+}
